81045995a83cd5fb042b0048885b1262e338c682
Brian L. Troutwine <brian.troutwine@adroll.com>
2015-03-16 15:19:46 -0700

    Scoot imports up to the top of the file.
    The location of these cause a build error on R16.
    
    Signed-off-by: Brian L. Troutwine <brian.troutwine@adroll.com>

--- include/triq.hrl
+++ include/triq.hrl
@@ -18,46 +18,12 @@
 %% limitations under the License.
 %%
 
--define(DELAY(X), fun()->X end).
--define(FORCE(X), (X)() ).
--define(DOMAIN_MODULE, triq_dom).
-
-%% properties
--define(FORALL(X,Gen,Property),
-        {'prop:forall', Gen, ??X, fun(X)-> begin Property end end, ??Property}).
--define(IMPLIES(Pre, Property),
-        {'prop:implies', Pre, ??Pre, ?DELAY( Property ), ??Property}).
--define(WHENFAIL(Action, Property),
-        {'prop:whenfail', ?DELAY(Action), ?DELAY(Property), ??Property}).
--define(TRAPEXIT(Property),
-        {'prop:trapexit', ?DELAY(Property), ??Property}).
--define(TIMEOUT(Limit,Property),
-        {'prop:timeout', Limit, ?DELAY(Property), ??Property}).
-
 %%
 %% import property functions
 %%
 -import(triq,
         [fails/1,
          check/1]).
-
-
-%% value domains
-
-%% LET is also defined by eunit; what to do?
--ifndef(LET).
--define(LET(X,Gen1,Gen2),
-        ?DOMAIN_MODULE:bind(Gen1, fun(X)->Gen2 end)).
--endif.
--define(LETSHRINK(X,Gen1,Gen2),
-        ?DOMAIN_MODULE:bindshrink(Gen1, fun(X)->Gen2 end)).
-
--define(SIZED(Size,Gen),
-        ?DOMAIN_MODULE:sized(fun(Size) -> Gen end)).
-
--define(SUCHTHAT(X,G,P),
-        ?DOMAIN_MODULE:suchthat(G, fun(X) -> P end)).
-
 
 %%
 %% import domain functions (a.k.a. generators)
@@ -112,6 +78,39 @@
          peek/1,
          domain/3,
          shrink_without_duplicates/1]).
+
+-define(DELAY(X), fun()->X end).
+-define(FORCE(X), (X)() ).
+-define(DOMAIN_MODULE, triq_dom).
+
+%% properties
+-define(FORALL(X,Gen,Property),
+        {'prop:forall', Gen, ??X, fun(X)-> begin Property end end, ??Property}).
+-define(IMPLIES(Pre, Property),
+        {'prop:implies', Pre, ??Pre, ?DELAY( Property ), ??Property}).
+-define(WHENFAIL(Action, Property),
+        {'prop:whenfail', ?DELAY(Action), ?DELAY(Property), ??Property}).
+-define(TRAPEXIT(Property),
+        {'prop:trapexit', ?DELAY(Property), ??Property}).
+-define(TIMEOUT(Limit,Property),
+        {'prop:timeout', Limit, ?DELAY(Property), ??Property}).
+
+%% value domains
+
+%% LET is also defined by eunit; what to do?
+-ifndef(LET).
+-define(LET(X,Gen1,Gen2),
+        ?DOMAIN_MODULE:bind(Gen1, fun(X)->Gen2 end)).
+-endif.
+-define(LETSHRINK(X,Gen1,Gen2),
+        ?DOMAIN_MODULE:bindshrink(Gen1, fun(X)->Gen2 end)).
+
+-define(SIZED(Size,Gen),
+        ?DOMAIN_MODULE:sized(fun(Size) -> Gen end)).
+
+-define(SUCHTHAT(X,G,P),
+        ?DOMAIN_MODULE:suchthat(G, fun(X) -> P end)).
+
 
 %%
 %% Enabling this (the default) does two things (similar to eunit).
